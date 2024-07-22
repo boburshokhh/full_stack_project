fa64c761b35747bbaeea28267c7c485a62a25884
Бобур <boburboy200216b@mail.ru>
2024-07-22 20:49:03 +0500

    firs-commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>{{ count }}</p>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    }
+  }
+};
+</script>
+
+<style scoped>
+button {
+  margin-top: 10px;
+  padding: 10px;
+  font-size: 16px;
+}
+</style>
